a1d12bb6ce901483703583dd2ebd6b17e1bf11e9
lurenjiamax <1932231507@outlook.com>
2022-08-09 22:56:42 +0800

    add mtk-feeds

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -24,7 +24,7 @@ addFeeds(){
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git custom https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
 addFeeds custom https://github.com/kiddin9/openwrt-packages.git
-
+addFeeds mtk https://github.com/Nanonona/mtk-openwrt-feeds.git
 # 添加软件包源
 # git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #新的argon主题 (custom已附带)
 # git clone --depth=1 https://github.com/llccd/openwrt-fullconenat.git package/openwrt-fullconenat #全锥形NAT (custom已附带)
